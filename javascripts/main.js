b462b7b8539e0d8a1ce7bfa9097bb4424536af46
Jeffrey Ko <jeffreyko@gmail.com>
2014-07-15 21:16:47 -0400

    re-layout

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,37 @@
-console.log('This would be the main JS file.');
+
+(function() {
+    angular.module('nvApp',['nvChart', 'ui.bootstrap'])
+        .config(['$locationProvider', function($locationProvider) {
+            $locationProvider.html5Mode(true);
+        }])
+        .directive('scrollto', [function(){
+            return function(scope, elm, attrs) {
+                elm.bind('click', function(e){
+                    e.preventDefault();
+                    if (attrs.href) {
+                        attrs.scrollto = attrs.href;
+                    }
+                    var top = $(attrs.scrollto).offset().top - 51;
+                    $('body').animate({ scrollTop: top }, 200);
+                });
+            };
+        }])
+        .controller('nvCtrl', ['$scope', function($scope) {
+            $scope.scrollTo = function(id) {
+                $location.hash(id);
+                $anchorScroll();
+            }
+
+            /*Basic Example*/
+            $scope.chartOptions = {
+                chartType: 'lineChart',
+                data: 'chartData'
+            };
+            $scope.chartData = [{
+                key: 'Series 1',
+                values:[{'x': 1, 'y' : 5},{'x': 2, 'y' : 10},{'x': 3, 'y' : 15}]
+            }];
+        }]);
+
+    prettyPrint();
+}())
